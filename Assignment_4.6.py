eaa0eb9a7f551ea8a9c710fe21e2ece46e51ad65
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 11:59:14 +0530

    Statistics Assignment 4.6

---
+++ Assignment_4.6.py
@@ -0,0 +1,22 @@
+Problem Statement: 4.6
+
+Two balls are drawn at random in succession without replacement from an urn
+containing 4 red balls and 6 black balls. Find the probabilities of all the possible outcomes.
+
+
+Solution:
+    
+   Total ways can be = 10 ∗ 3
+   
+   
+you pick one ball after another. There a three ways:
+
+rbb
+brb
+bbr
+
+Each of them have the probability of:
+
+4/10⋅6/9⋅5/8
+
+And in total 3⋅4/10⋅6/9⋅5/8
